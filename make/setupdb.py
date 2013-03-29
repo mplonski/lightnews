3b431a29527777747bbc81dbe1f6392678a94306
Maciej Plonski <m.plonski@sokoli.pl>
2013-03-29 20:07:09 +0100

    huh?

--- make/setupdb.py
+++ make/setupdb.py
@@ -9,17 +9,16 @@ def setupdb(url):
 	c = conn.cursor()
 
 	c.execute('CREATE TABLE options  (name text primary key, val text)')
-	c.execute('CREATE TABLE servers  (id integer primary key, name text, username text, password text)')
+	c.execute('CREATE TABLE servers  (id integer primary key, name text, auth int)') # 0 -- required auth
 	c.execute('CREATE TABLE groups   (id integer primary key, server_id integer, name text, cache integer, count integer, first integer, last integer)')
 	c.execute('CREATE TABLE articles (id integer primary key, art_id integer, server_id integer, group_id integer, title text, body text)')
 	c.execute('CREATE TABLE read_art (id integer, group_id integer)') # 0 -- read, 1 -- to read
 	conn.commit()
 
 	if DEBUG == 1:
-
-		c.execute("INSERT INTO servers VALUES (NULL, 'news.gmane.org', NULL, NULL)")
+		c.execute("INSERT INTO servers VALUES (NULL, 'news.gmane.org', 1)")
 		c.execute("INSERT INTO groups VALUES (NULL, 1, 'gmane.comp.python.committers', 5, 0, 0, 0)")
-		c.execute("INSERT INTO servers VALUES (NULL, 'test2.test', NULL, NULL)")
+		c.execute("INSERT INTO servers VALUES (NULL, 'test2.test', 1)")
 		c.execute("INSERT INTO groups VALUES (NULL, 2, 'test_s2.test', 0, 200, 100, 300)")
 		conn.commit()
 
